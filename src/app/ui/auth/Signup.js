1c4054928db52583f827f9cffeae94e2f08d606a
Vijay-mehta <infovijay1998@gmail.com>
2024-06-10 07:34:00 +0530

    signup form

--- src/app/ui/auth/Signup.js
+++ src/app/ui/auth/Signup.js
@@ -1,7 +1,23 @@
+'use client'
 const Signup =()=>{
+
+    const handleChange=()=>{
+        
+    }
     return(
-        <>
-        Signup</>
+        <div className=" flex justify-center  text-black bg-white my-10 ">
+        <div className="   w-[1100px]">
+            <h1 className=" font-bold text-xl mb-4 m-2">Signup Form</h1>
+        <form className="shadow-md rounded-sm p-5">
+      
+        <input type="file" placeholder="Upload Your Photo" name="file" onChange={(e)=>handleChange(e.target.value)} className="px-3 m-4 py-1 border border-gray-300  w-full hover:border-blue-200  text-xl"   />
+        <input type="text" placeholder="Enter Your Name" name="name" onChange={(e)=>handleChange(e.target.value)} className="px-3 m-4 py-1  border border-gray-300  w-full hover:border-blue-200  text-xl"   />
+        <input type="email" placeholder="Enter Your Email" name="eamil" onChange={(e)=>handleChange(e.target.value)} className="px-3 m-4 py-1 border border-gray-300  w-full hover:border-blue-200  text-xl"   />
+        <input type="password" placeholder="Enter Your Password" name="password" onChange={(e)=>handleChange(e.target.value)} className="px-3 m-4     py-1 border border-gray-300  w-full hover:border-blue-200  text-xl"   />
+        <div className=" flex  mt-3 m-2"><div className="bg-[#f75d34] text-white px-4 py-2 mr-2"><button type="submit"> Signup</button></div><div className="bg-[#f75d34] text-white px-4 py-2"><button type="submit">Login</button></div></div>
+        </form>
+        </div>
+        </div>
     )
 }
 
--- src/app/ui/home/HomeLogo.js
+++ src/app/ui/home/HomeLogo.js
@@ -7,7 +7,7 @@ const HomeLogo=()=>{
     return(
         <div className=" flex items-center  bg-white  text-black  shadow-md  p-2  rounded-md">
       <FaCarSide   className=" h-20 w-20 text-[#f75d34]"/><p className=" pl-2  font-bold text-xl ">CarVista</p>
-      <PiArrowRightLight className=" w-14  h-9"/>
+      <PiArrowRightLight className=" w-10  h-9"/>
 
     </div>
     )
