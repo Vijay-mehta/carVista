a47d27ce4735d8a2c5ec5429863b8c9319acc683
Vijay-mehta <infovijay1998@gmail.com>
2024-06-15 18:35:00 +0530

    add home button

--- src/app/ui/home/HomeLogo.js
+++ src/app/ui/home/HomeLogo.js
@@ -1,42 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaCarSide } from "react-icons/fa";
+import { PiPoliceCarDuotone } from "react-icons/pi";
 import { PiArrowRightLight } from "react-icons/pi";
 
-const HomeLogo = ({ devicePixelRatio }) => {
+const HomeLogo = ({ carName }) => {
   return (
     <>
-      {devicePixelRatio > 2 ? (
-        devicePixelRatio > 3 ? (
-          <>
-            <div className=" flex items-center  bg-[#beac86] shadow-md  rounded-md px-6 py-2">
-              <Link href="/store/c">
-                <p className=" pl-2  font-bold text-xl text-white">See More</p>
-              </Link>
-            </div>
-          </>
-        ) : (
-          <>
-           <div className=" flex items-center  bg-[#beac86] shadow-md  rounded-md px-6 py-2">
-              <Link href="/store/c">
-                <p className=" pl-2  font-bold text-xl text-white">See More</p>
-              </Link>
-            </div>
-          </>
-        )
-      ) : (
-        <>
-        <Link href="/store/c">
-          <div className=" flex items-center  bg-[#beac86] shadow-md  rounded-md px-4 py-2  ">
-            <FaCarSide className=" h-12 w-12 text-[#f75d34]" />
-            
-              <p className=" pl-2  font-bold text-xl text-white">CarVista</p>
-          
-            <PiArrowRightLight className=" text-white w-10 h-10 ml-3 md:w-10  md:h-9" />
-          </div>
-          </Link>
-        </>
-      )}
+      <Link href="/store/c" >
+        <div className=" flex items-center  bg-[#fafafa] shadow-md  p-4  md:px-6 py-2 ml-3 md:ml-5 ">
+          <PiPoliceCarDuotone className=" h-12 w-12 text-black " />
+
+          <p className=" pl-2  font-bold text-sm text-black">{carName}</p>
+
+          {/* <PiArrowRightLight className=" text-white w-10 h-10 ml-3 md:w-10  md:h-9" /> */}
+        </div>
+      </Link>
     </>
   );
 };
--- src/app/ui/home/Main.js
+++ src/app/ui/home/Main.js
@@ -1,28 +1,25 @@
 'use client';
 
 import Image from "next/image";
+import HomeLogo from "./HomeLogo";
 
 const Main = () => {
-
-
-
- 
-
   return (
     <div className="relative w-full h-[calc(100vh-1rem)] md:min-h-screen mt-3">
-    
-       
-        <Image
-          src="/home/car.jpg"
-          alt="home page image"
-          quality={100}
-          layout="fill"
-          objectFit="cover"
-          priority={true}
-          className="object-center"
-        />
-      
-     
+      <Image
+        src="/home/car.jpg"
+        alt="home bg-image"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }}
+        className="object-center"
+      />
+     <div className=" absolute top-44 right-6  md:bottom-96 md:left-20 flex"><HomeLogo carName="Buy Car"/> 
+     <HomeLogo carName="Service Car"/></div>
+
     </div>
   );
 };
