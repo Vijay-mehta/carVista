89f70e374bfa2389e65fae8e9c4f6ede7785e0fb
Vijay-mehta <infovijay1998@gmail.com>
2024-06-16 22:06:04 +0530

    old

--- src/app/ui/home/HomeLogo.js
+++ src/app/ui/home/HomeLogo.js
@@ -6,7 +6,7 @@ import { PiArrowRightLight } from "react-icons/pi";
 const HomeLogo = ({ carName }) => {
   return (
     <>
-      <Link href="/store/c" >
+      <Link href="/c" >
         <div className=" flex items-center  bg-[#fafafa] shadow-md  p-4  md:px-6 py-2 ml-3 md:ml-5 ">
           <PiPoliceCarDuotone className=" h-12 w-12 text-black " />
 
