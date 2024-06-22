7968fdbfbc85337b09d985affd31c2ca4a988a10
Vijay-mehta <infovijay1998@gmail.com>
2024-06-22 17:56:53 +0530

    add loader

--- src/app/(store)/c/page.js
+++ src/app/(store)/c/page.js
@@ -1,20 +1,21 @@
 "use client";
 import { useInternalApiService } from "@/app/hook/useInternalApiService";
 import CarList from "@/app/ui/car/CartList";
+import Loader from "@/app/ui/loader";
 import { useEffect, useState } from "react";
 
 const UserCar = () => {
   const [fetchCar, fetchCarResult, fetchCarInProgress, fetchCarError] =
     useInternalApiService("api/car/getcar", "GET");
 
-  useEffect( () => {
+  useEffect(() => {
     fetchCar();
   }, []);
   return (
     <div className=" bg-white">
-      <CarList carData={fetchCarResult} />
-    </div>
+     <div className=" relative">{fetchCarInProgress ? <div className=" absolute top-64  left-28 md:top-[450px] md:left-[600px]"><Loader /> </div>:<div><CarList carData={fetchCarResult} /> </div>} </div>
+      </div>
   );
 };
 
-export default UserCar;
+        export default UserCar;
